8af167be8efdc675549cec7b8a53b1f897f11afe
Ruthvik Peddawandla <Ruthvik@Ruthviks-MacBook-Pro.local>
2016-03-31 01:24:26 -0700

    Temporal extraction implemented

--- enrich.py
+++ enrich.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from utils import print_import_error
+
 try:
     import pysolr
     from pysolr import SolrError
@@ -11,8 +12,10 @@ try:
 except ImportError as e:
     print_import_error('tika', e)
     exit(1)
+
 import argparse
 from geo_enrich import extract_geo_from_doc
+from temporal_enrich import extract_temporal_from_doc
 
 __author__ = 'Lorraine Sposto'
 
@@ -25,13 +28,13 @@ GEO_KEY = 'geo'
 TEMPORAL_KEY = 'temp'
 
 SOLR_SOURCE = 'http://polar.usc.edu/solr/polar'
-SOLR_DEST = 'http://polar.usc.edu/solr/geo_enriched'
-# SOLR_SOURCE = 'http://localhost:8983/solr/collection1'
-# SOLR_DEST = 'http://localhost:8983/solr/geo'
+SOLR_DEST_GEO = 'http://polar.usc.edu/solr/geo'
+SOLR_DEST_TEMP = 'http://polar.usc.edu/solr/temporal'
 TIKA_SERVER = 'http://localhost:9998/tika'
 
 def process_solr_docs(name, start, rows, rounds, src, dest, tika):
     queries_made = 0
+    num_total_docs = 0
     num_total_successful = 0
     num_total_failure = 0
 
@@ -53,20 +56,21 @@ def process_solr_docs(name, start, rows, rounds, src, dest, tika):
             'rows': rows
         })
 
-        geo_docs = []
+        enriched_docs = []
         for doc in r.docs:
-            geo_enriched_doc = None
+            num_total_docs += 1
+            enriched = None
             if name == GEO_KEY:
-                geo_enriched_doc = extract_geo_from_doc(doc, tika)
+                enriched = extract_geo_from_doc(doc, tika)
             elif name == TEMPORAL_KEY:
-                pass
+                enriched = extract_temporal_from_doc(doc)
 
-            if geo_enriched_doc is not None:
-                geo_docs.append(geo_enriched_doc)
+            if enriched is not None:
+                enriched_docs.append(enriched)
                 num_total_successful += 1
 
         try:
-            solr_dest.add(geo_docs)
+            solr_dest.add(enriched_docs)
             print('Indexed', num_total_successful, 'docs')
         except SolrError as e:
             failed_at.append(start)
@@ -77,6 +81,7 @@ def process_solr_docs(name, start, rows, rounds, src, dest, tika):
 
     print('\n---------------')
     print('Hit Solr', queries_made, 'times')
+    print('Total documents found: ', num_total_docs)
     print('Successfully indexed', num_total_successful, 'docs')
     print('Failed at starts:', failed_at)
 
@@ -89,14 +94,14 @@ if __name__ == "__main__":
     temp_parser = subparser.add_parser(TEMPORAL_KEY)
 
     geo_parser.add_argument('-s', '--src', dest='source', default=SOLR_SOURCE, help='url of source solr core')
-    geo_parser.add_argument('-d', '--dest', dest='dest', default=SOLR_DEST, help='url of destination solr core')
+    geo_parser.add_argument('-d', '--dest', dest='dest', default=SOLR_DEST_GEO, help='url of destination solr core')
     geo_parser.add_argument('-t', '--tika', dest='tika', default=TIKA_SERVER, help='url of running tika server')
     geo_parser.add_argument('--start', dest='start', type=int, default=0, help='start row to query solr, default 0')
     geo_parser.add_argument('--rows', dest='rows', type=int, default=1000, help='number of rows to query from solr, default 1000')
     geo_parser.add_argument('--rounds', dest='rounds', type=int, default=1, help='number of times to query from solr, default 1')
 
     temp_parser.add_argument('-s', '--src', dest='source', default=SOLR_SOURCE, help='url of source solr core')
-    temp_parser.add_argument('-d', '--dest', dest='dest', default=SOLR_DEST, help='url of destination solr core')
+    temp_parser.add_argument('-d', '--dest', dest='dest', default=SOLR_DEST_TEMP, help='url of destination solr core')
     temp_parser.add_argument('-t', '--tika', dest='tika', default=TIKA_SERVER, help='url of running tika server')
     temp_parser.add_argument('--start', dest='start', type=int, default=0, help='start row to query solr, default 0')
     temp_parser.add_argument('--rows', dest='rows', type=int, default=1000, help='number of rows to query from solr, default 1000')
@@ -114,6 +119,5 @@ if __name__ == "__main__":
     rounds = args.rounds
 
     process_solr_docs(name, start, rows, rounds, solr_src, solr_dest, tika)
-
 
     print('Exiting...')

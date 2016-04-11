2c7e0b44005105eebbbdb2b2aa4d54d51a9819c3
Ruthvik Peddawandla <Ruthvik@MacBook-Pro.local>
2016-04-10 18:16:51 -0700

    extract-temporal-data-from-relative-expressions

--- temporal_enrich.py
+++ temporal_enrich.py
@@ -7,15 +7,29 @@ except ImportError as e:
     print_import_error('pysolr', e)
     exit(1)
 try:
-    from nltk_contrib import timex
+    from nltk_contrib.timex import *
 except ImportError as e:
     print_import_error('nltk_contrib.timex', e)
     exit(1)
 import copy
+import re
+import string
 from datetime import datetime
 
 
 def extract_temporal_from_doc(doc):
+    """
+    Uses the NLTK timex.py module to extract temporal tags from the document and
+    returns an enriched/updated copy of the document.
+
+    List of extracted/formatted datestrings is added to 'extracted-dates' field
+    in enriched document.
+
+    :param doc: A document from solr, as a dict
+
+    :return: Updated document, as a dict, or None if error
+    """
+
     enriched = copy.deepcopy(doc)
     extracted_date_list = []
     master_list = []
@@ -31,51 +45,87 @@ def extract_temporal_from_doc(doc):
         master_list += doc['title']
 
     for content in master_list:
-        temporal_tags = timex.tag(content)
-        for tag in temporal_tags:
-            if tag not in extracted_date_list:
+        temporal_tags = tag(content)
+        for tagged_text in temporal_tags:
+            if tagged_text not in extracted_date_list:
 
-                # just a year
-                if isinstance(tag, str) and len(tag) == 4:
-                    # date_list = get year month day as list
-                    datestring_tag = get_date_string_from_ymd(year=int(tag))
-                    # print(datestring_tag)
-                    extracted_date_list.append(datestring_tag)
-                elif isinstance(tag, dict):
-                    day = None
-                    month = None
-                    year = None
-                    if 'day' in tag:
-                        day = tag['day']
+                if isinstance(tagged_text, dict):
+                    temp_day = None
+                    temp_month = None
+                    temp_year = None
+                    if 'day' in tagged_text:
+                        temp_day = tagged_text['day']
                     else:
-                        day = 1
-                    if 'month' in tag:
-                        month = tag['month']
+                        temp_day = 1
+                    if 'month' in tagged_text:
+                        temp_month = tagged_text['month']
                     else:
-                        month = 1
-                    if 'year' in tag:
-                        year = tag['year']
-                    print(">> Parsing day {0}, month {1}, year {2}".format(day, month, year))
-                    date_str = get_date_string_from_ymd(year=year, month=month, day=day)
+                        temp_month = 1
+                    if 'year' in tagged_text:
+                        temp_year = tagged_text['year']
+                    # print(">> Parsing day {0}, month {1}, year {2}".format(temp_day, temp_month, temp_year))
+                    date_str = get_datestring_from_ymd(year=temp_year, month=temp_month, day=temp_day)
                     extracted_date_list.append(date_str)
 
-                # TODO: don't do anything for now because Solr doesn't accept non formatted
-                # date strings (so make function to get a date format for the string tag)
-                # else:
-                    # extracted_date_list.append(tag)
+                # just a year
+                elif tagged_text.isdigit() and len(tagged_text) == 4:
+                    # date_list = get year month day as list
+                    datestring_tag = get_datestring_from_ymd(year=int(tagged_text))
+                    extracted_date_list.append(datestring_tag)
 
-    print('Extracted dates', extracted_date_list)
+                # If we're here the tag returned is a relative date expression (e.g.
+                # 'this year', 'today', etc.) - we f
+                else:
+                    text = re.sub(tagged_text + '(?!</TIMEX2>)', '<TIMEX2>' + tagged_text + '</TIMEX2>', tagged_text)
+                    try:
+                        grounded_text = ground(text, gmt())
+                        gr_dates = re.findall(r'"([^"]*)"', grounded_text)
+
+                        for date in gr_dates:
+                            ymd_list = date.split('-')
+
+                            year = 0
+                            month = 0
+                            day = 0
+
+                            if len(ymd_list) > 0:
+                                year = int(ymd_list[0])
+                            if len(ymd_list) > 1:
+                                month = int(ymd_list[1])
+                            if len(ymd_list) > 2:
+                                day = int(ymd_list[2])
+
+                            datestring_tag = get_datestring_from_ymd(year=year, month=month, day=day)
+                            extracted_date_list.append(datestring_tag)
+
+                    except Exception as e:
+                        print(e.message)
+
+    # print('Extracted dates', extracted_date_list)
     if (len(extracted_date_list)):
         enriched['extracted_dates'] = extracted_date_list
 
     return enriched
 
 
-def get_date_string_from_ymd(year=None, month=None, day=None):
-    if day is None:
-        day = 1
-    if month is None:
+def get_datestring_from_ymd(year=None, month=None, day=None):
+    """
+    Format of datestring added to enriched document is: 'YYYY-MM-DDTHH-mm-ssZ'.
+    Note that this is the format that Solr requires.
+
+    :param year: (Optional) Year extracted from document, as an integer.
+    :param month: (Optional) Month of year extracted from document, as an integer.
+    :param day: (Optional) Day of month extracted from document, as an integer.
+
+    :return: Default year = 200, month = 1, day = 1; otherwise datestring of parameters
+    formatted to Solr specifications.
+    """
+    if year is None or year <= 1900 or year >= 2200:
+        year = 2000
+    if month is None or month == 0:
         month = 1
+    if day is None or day == 0:
+        day = 1
 
     try:
         year = int(year)
@@ -84,9 +134,6 @@ def get_date_string_from_ymd(year=None, month=None, day=None):
         print ("Year and day must be integers.")
         print(e.message)
         return None
-
-    if year is None or year <= 1900:
-        year = 2000
 
     if isinstance(month, (str, unicode)):
         if len(month) == 3:
@@ -104,10 +151,6 @@ def get_date_string_from_ymd(year=None, month=None, day=None):
             dt = datetime(day=month, month=day, year=year)
         except Exception as e:
             print(e.message)
-            # print()
             return None
 
-    ret = dt.strftime("%Y-%m-%dT%H:%M:%SZ")
-    # print("Parsed", ret)
-    # print()
-    return ret
+    return dt.strftime("%Y-%m-%dT%H:%M:%SZ")
